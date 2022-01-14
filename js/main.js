3eeaf4568c05606a7930d1b950877d44b4f0c1a2
Md.Jahid Hasan Noyon <96732080+jahidhasasn@users.noreply.github.com>
2022-01-14 20:44:53 +0600

    first-commit

--- js/main.js
+++ js/main.js
@@ -3,4 +3,12 @@ $(document).ready(function(){
         delay: 10,
         time: 1000
     });
-})
+
+    if($('.progress-line').length) {
+        $('.progress-line').appear(function(){
+            var el = $(this);
+            var percent = el.data('width');
+            $(el).css('width', percent+'%');
+        }, {accY:0});
+        }
+});
